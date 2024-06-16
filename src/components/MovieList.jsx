a9264fc0c7e25b16bacc80e68f783d8335dc4f17
Himanshu Singh Mandloi <mandloihimanshusingh@gmail.com>
2024-06-16 23:37:13 +0530

    added the css to the project and it's completed

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import MovieCard from '../pages/MovieCard';
+import MovieCard from "./MovieCard";
+import styles from "./MoviesList.module.css";
 
-function MovieList({data}) {
 
-  const {movieApiResponse, error, isError} = data;
+
+function MovieList({ data }) {
+  const { movieApiResponse, error, isError } = data;
+  // console.log(movieApiResponse);
 
   if (isError) {
     return <h1>{error}</h1>
   }
 
-  if (movieApiResponse && movieApiResponse.Response === 'False') {
-    return <h1> {movieApiResponse.Error || "No Result Found"} </h1>
+  if (movieApiResponse && movieApiResponse.Response === "False") {
+    return <h1>{movieApiResponse.Error || "No Result Found"}</h1>;
   }
 
   return (
-    <div>
-      {movieApiResponse.Search.map((movie) => <MovieCard key={movie.imdbID} {...movie}/>)}
+    <div className={`container ${styles.moviesList}`}>
+      {movieApiResponse.Search.map((movie) => (
+        <MovieCard  key={movie.imdbID} {...movie}/>
+      ))}
     </div>
-  )
+  );
 }
 
-export default MovieList
+export default MovieList;
--- src/components/SearchFrom.jsx
+++ src/components/SearchFrom.jsx
@@ -1,12 +1,15 @@
 import { Form } from "react-router-dom"
+import styles from "./SearchForm.module.css";
 
 
 
 function SearchFrom({searchTerm}) {
   return (
-    <Form method="GET">
+    <Form method="GET"  className={`container ${styles.form}`}>
+
       <input type="text" name="search" id="search" required  defaultValue={searchTerm}/>
       <button type="submit"> Search </button>
+      
     </Form>
   )
 }
